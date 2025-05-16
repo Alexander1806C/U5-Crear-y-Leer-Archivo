d102c92a083875f12f410ede744e220ade84f917
JOSUE ALEXANDER CORREA ORTIZ <I24050375@monclova.tecnm.mx>
2025-05-15 19:13:23 -0600

    Agregar archivos de proyecto.

---
+++ tercera unidad 3 actividad 1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace tercera_unidad_3_actividad_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTNcrearfichero = new System.Windows.Forms.Button();
+            this.BTNforma2decrear = new System.Windows.Forms.Button();
+            this.BTNleerarchivo1 = new System.Windows.Forms.Button();
+            this.BTNleerFinal = new System.Windows.Forms.Button();
+            this.BTNleerarchivodowhile = new System.Windows.Forms.Button();
+            this.BtnAbrir2 = new System.Windows.Forms.Button();
+            this.BNTmodificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTNcrearfichero
+            // 
+            this.BTNcrearfichero.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNcrearfichero.Location = new System.Drawing.Point(11, 11);
+            this.BTNcrearfichero.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTNcrearfichero.Name = "BTNcrearfichero";
+            this.BTNcrearfichero.Size = new System.Drawing.Size(97, 46);
+            this.BTNcrearfichero.TabIndex = 0;
+            this.BTNcrearfichero.Text = "Crear fichero";
+            this.BTNcrearfichero.UseVisualStyleBackColor = true;
+            this.BTNcrearfichero.Click += new System.EventHandler(this.BTNcrearfichero_Click);
+            // 
+            // BTNforma2decrear
+            // 
+            this.BTNforma2decrear.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNforma2decrear.Location = new System.Drawing.Point(114, 11);
+            this.BTNforma2decrear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTNforma2decrear.Name = "BTNforma2decrear";
+            this.BTNforma2decrear.Size = new System.Drawing.Size(118, 46);
+            this.BTNforma2decrear.TabIndex = 1;
+            this.BTNforma2decrear.Text = "Forma 2 de crear";
+            this.BTNforma2decrear.UseVisualStyleBackColor = true;
+            this.BTNforma2decrear.Click += new System.EventHandler(this.BTNforma2decrear_Click);
+            // 
+            // BTNleerarchivo1
+            // 
+            this.BTNleerarchivo1.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNleerarchivo1.Location = new System.Drawing.Point(236, 11);
+            this.BTNleerarchivo1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTNleerarchivo1.Name = "BTNleerarchivo1";
+            this.BTNleerarchivo1.Size = new System.Drawing.Size(105, 46);
+            this.BTNleerarchivo1.TabIndex = 2;
+            this.BTNleerarchivo1.Text = "Leer archivo";
+            this.BTNleerarchivo1.UseVisualStyleBackColor = true;
+            this.BTNleerarchivo1.Click += new System.EventHandler(this.BTNleerarchivo1_Click);
+            // 
+            // BTNleerFinal
+            // 
+            this.BTNleerFinal.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNleerFinal.Location = new System.Drawing.Point(345, 11);
+            this.BTNleerFinal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTNleerFinal.Name = "BTNleerFinal";
+            this.BTNleerFinal.Size = new System.Drawing.Size(104, 46);
+            this.BTNleerFinal.TabIndex = 3;
+            this.BTNleerFinal.Text = "Leer archivo Final";
+            this.BTNleerFinal.UseVisualStyleBackColor = true;
+            this.BTNleerFinal.Click += new System.EventHandler(this.BTNleerFinal_Click);
+            // 
+            // BTNleerarchivodowhile
+            // 
+            this.BTNleerarchivodowhile.BackColor = System.Drawing.Color.Gainsboro;
+            this.BTNleerarchivodowhile.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNleerarchivodowhile.Location = new System.Drawing.Point(453, 11);
+            this.BTNleerarchivodowhile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BTNleerarchivodowhile.Name = "BTNleerarchivodowhile";
+            this.BTNleerarchivodowhile.Size = new System.Drawing.Size(113, 46);
+            this.BTNleerarchivodowhile.TabIndex = 4;
+            this.BTNleerarchivodowhile.Text = "Leer archivo do while";
+            this.BTNleerarchivodowhile.UseVisualStyleBackColor = false;
+            this.BTNleerarchivodowhile.Click += new System.EventHandler(this.BTNleerarchivodowhile_Click);
+            // 
+            // BtnAbrir2
+            // 
+            this.BtnAbrir2.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAbrir2.Location = new System.Drawing.Point(71, 97);
+            this.BtnAbrir2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnAbrir2.Name = "BtnAbrir2";
+            this.BtnAbrir2.Size = new System.Drawing.Size(100, 46);
+            this.BtnAbrir2.TabIndex = 5;
+            this.BtnAbrir2.Text = "Abrir 2 ";
+            this.BtnAbrir2.UseVisualStyleBackColor = true;
+            this.BtnAbrir2.Click += new System.EventHandler(this.BtnAbrir2_Click);
+            // 
+            // BNTmodificar
+            // 
+            this.BNTmodificar.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BNTmodificar.Location = new System.Drawing.Point(215, 97);
+            this.BNTmodificar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BNTmodificar.Name = "BNTmodificar";
+            this.BNTmodificar.Size = new System.Drawing.Size(100, 46);
+            this.BNTmodificar.TabIndex = 6;
+            this.BNTmodificar.Text = "Modificar";
+            this.BNTmodificar.UseVisualStyleBackColor = true;
+            this.BNTmodificar.Click += new System.EventHandler(this.BNTmodificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.BNTmodificar);
+            this.Controls.Add(this.BtnAbrir2);
+            this.Controls.Add(this.BTNleerarchivodowhile);
+            this.Controls.Add(this.BTNleerFinal);
+            this.Controls.Add(this.BTNleerarchivo1);
+            this.Controls.Add(this.BTNforma2decrear);
+            this.Controls.Add(this.BTNcrearfichero);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTNcrearfichero;
+        private System.Windows.Forms.Button BTNforma2decrear;
+        private System.Windows.Forms.Button BTNleerarchivo1;
+        private System.Windows.Forms.Button BTNleerFinal;
+        private System.Windows.Forms.Button BTNleerarchivodowhile;
+        private System.Windows.Forms.Button BtnAbrir2;
+        private System.Windows.Forms.Button BNTmodificar;
+    }
+}
+
---
+++ tercera unidad 3 actividad 1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace tercera_unidad_3_actividad_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTNcrearfichero_Click(object sender, EventArgs e)
+        {
+           //StreamWriter es la clase que representa a un fichero de texto 
+           //en el que podemos escribir informacion 
+           //El fichero de texto lo creamos con el metodo ( estatico ) CreateText que pertenece a la clase file 
+           //Para escribir en el fichero , empleamos write y writeline 
+           //Close para cerrar el archivo , o podria quedar algun dato sin guardar
+            
+            StreamWriter fichero;
+            fichero = File.CreateText("fichero1.txt");
+            fichero.WriteLine("  ");
+            fichero.Write(" primer paso ");
+            fichero.Write("\r\n");
+            fichero.WriteLine("  ");
+            fichero.Close();         
+        
+        
+        
+        }
+
+        private void BTNforma2decrear_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter fichero = new StreamWriter("fichero1.txt"))
+            {
+                fichero.WriteLine("Este codigo es mas compacto");
+                fichero.WriteLine("jesus");
+                fichero.WriteLine("carlos");
+            }
+        }
+
+        private void BTNleerarchivo1_Click(object sender, EventArgs e)
+        {
+            StreamReader fichero;
+            string linea;
+            fichero = File.OpenText("fichero1.txt");
+            linea = fichero.ReadLine();
+            Console.WriteLine(linea);
+           // Console.WriteLine(fichero.ReadLine());
+           //Console.WriteLine(fichero.ReadLine());
+            fichero.Close();
+        }
+
+        private void BTNleerFinal_Click(object sender, EventArgs e)
+        {
+            StreamReader fichero;
+            fichero = File.OpenText("fichero1.txt");
+            while(!fichero.EndOfStream)
+                Console.WriteLine(fichero.ReadLine());
+            fichero.Close();
+            
+
+            
+        }
+
+        private void BTNleerarchivodowhile_Click(object sender, EventArgs e)
+        {
+            StreamReader fichero;
+            string linea;
+            fichero = File.OpenText("fichero1.txt");
+            do 
+            { 
+             linea = fichero.ReadLine();
+                if (linea !=  null)
+                    Console.WriteLine(linea);
+            
+            }
+            while(linea != null);
+            fichero.Close();
+        }
+
+        private void BNTmodificar_Click(object sender, EventArgs e)
+        {
+            StreamWriter fichero;
+            fichero = File.CreateText("fichero1.txt");
+            fichero.WriteLine("primera prueba");
+            fichero.Close();
+
+            fichero = File.AppendText("fichero1.txt");
+            fichero.WriteLine("Segunda prueba");
+            fichero.Close();
+           
+        }
+
+        private void BtnAbrir2_Click(object sender, EventArgs e)
+        {
+            using (StreamReader fichero = File.OpenText("fichero1.txt"))
+            {
+                string linea;
+                linea = fichero.ReadLine();
+                Console.WriteLine(linea);
+                Console.WriteLine(fichero.ReadLine());
+                Console.WriteLine(fichero.ReadLine());
+            }
+        }
+    }
+}
